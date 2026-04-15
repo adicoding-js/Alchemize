78ccbabd3d6bbc33f3432059b3cf900229583eca
Utkarsh Chandel <utkarshchandel2012@gmail.com>
2026-04-15 12:18:53 +0530

    Initial Commit

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,9 @@
+<div class="nav w-screen h-24 bg-grad absolute flex items-center pt-4 pl-10 justify-between pr-10">
+        <img src="./Alchemist.webp" alt="" class="logo size-20">
+        <div class="links text-gray-300">Links Coming Shortly...</div>
+</div>
+<style>
+    .bg-grad{
+        background: linear-gradient(180deg, #00000000 20% , #000000 80%);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Nav from '$lib/Nav.svelte';
+</script>
+<Nav />
+<div class="absolute center-ovr w-screen h-screen flex items-center justify-center top-0">
+<div class="alchemy">
+    <h1 class=" text-red-500 text-5xl font-bold text-center mt-10 flex justify-center items-center">
+        <img src="./Alchemist.webp" alt="" class="logo size-20">
+        <span class="font alchemizefont">Alchemize</span>
+    </h1>
+    <div class="description">
+        <div class="flex items-center justify-center"><p class="text-gray-300 text-justify mt-10 text-lg w-4/5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique blanditiis velit natus, nisi accusamus quibusdam deleniti doloremque, possimus adipisci, eaque odio incidunt labore nemo at molestiae minus iusto ea inventore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt totam sapiente blanditiis molestiae eius. Ea laudantium reprehenderit ab expedita cum molestias doloremque? Sequi harum excepturi quae magnam vitae dolore minus.</p></div>
+    </div>
+</div>
+    
+</div>
+<div class="absolute -z-10 w-screen h-screen"><div class="img absolute -z-20 w-full h-full"></div><div class="ovr h-full w-screen -z-10"></div></div>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap');
+    .img {
+        background-image: url('./bg.png');
+        background-size: cover;
+        background-position: center;
+    }
+    .ovr{
+        background: rgba(0, 0, 0, 0.541);
+    }
+    .alchemy{
+        background: radial-gradient(#00000028, #000000);
+        height: 500px;
+        width: 600px;
+        border: 1px solid #161616;
+        border-radius: 30px;
+    }
+    .alchemizefont{
+        font-family: 'Dancing Script', cursive;
+    }
+</style>
