d749a8b460ccfb29019ef874420e080fe71007a2
Utkarsh Chandel <utkarshchandel2012@gmail.com>
2026-05-24 12:53:52 +0530

    FIX:HTML Block replaced with Render Block

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -72,6 +72,9 @@
 			>
 		</div>
 	{/if}
-	{@render children()}
+	{#if isAllowed}
+			{@render children()}
+
+	{/if}
 </div>
 <!-- <div class='bg bg-[url("/bg2.webp")] w-screen h-screen bg-cover absolute top-0 left-0 z-[-1]'></div> -->
