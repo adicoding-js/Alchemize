272da17484386856a76b18050c44248ed4017494
Utkarsh Chandel <utkarshchandel2012@gmail.com>
2026-06-10 16:39:47 +0530

    New bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,9 +58,9 @@
 			.then(data => (rsvpCount = data.count))
 	})
 </script>
-
+<div class="fixed -z-20 bg-[url('/new.png')] bg-cover bg-center w-screen h-screen blur-xl"></div>
 <div
-	class="relative min-h-screen w-full bg-gradbg text-zinc-100 font-mono tracking-wide selection:bg-primary selection:text-primary-foreground overflow-x-hidden"
+	class="relative min-h-screen w-full bg-[linear-gradient(to_bottom,#00000030_20%,#000c_70%)] text-zinc-100 font-mono tracking-wide selection:bg-primary selection:text-primary-foreground overflow-x-hidden"
 >
 	<div class="fixed inset-0 bg-black/25 z-0 pointer-events-none"></div>
 
