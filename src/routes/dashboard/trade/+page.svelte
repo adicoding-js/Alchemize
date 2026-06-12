1f85aa2cf6e3dd12cbaca069e4765d402fbf48cd
CoolCream <mahasankarshant@gmail.com>
2026-06-12 16:50:47 +0530

    rounded-md on dashboard and trade UI

--- src/routes/dashboard/trade/+page.svelte
+++ src/routes/dashboard/trade/+page.svelte
@@ -5,7 +5,7 @@
 	import { currenciesToPotionMix } from "$lib/utils"
 	import { invalidateAll } from "$app/navigation"
 	let { data } = $props()
-	import {toast} from "svelte-sonner"
+	import { toast } from "svelte-sonner"
 	let redstoneAmount = $state(0)
 	let glowstoneAmount = $state(0)
 	let aquaRegiaAmount = $state(0)
@@ -69,8 +69,10 @@
 	}
 </script>
 
-<main class="w-full h-screen bg-gradbg">
-	<div class="fixed inset-0 bg-black/50 z-10"></div>
+<main
+	class="w-full h-screen bg-gradbg selection:bg-primary selection:text-primary-foreground overflow-x-hidden"
+>
+	<div class="fixed inset-0 bg-black/20 z-10"></div>
 	<div
 		class="relative h-full w-full p-5 items-center justify-center flex-col flex z-50 gap-y-5"
 	>
@@ -93,120 +95,139 @@
 				<!-- sub-cards container -->
 				<div class="owned-stones grid grid-rows-3 gap-y-3 h-full w-full">
 					<!-- stone-1's card -->
-					<div
-						class="px-4 py-3 bg-white/10 border-2 border-white/10 rounded-xl backdrop-blur-2xl flex items-center justify-between gap-x-4"
-					>
-						<img
-							src="/alch-redstone.png"
-							alt="Redstone"
-							class="w-14 h-14 object-contain p-3 bg-red-600/20 border border-red-500/30 rounded-xl"
-						/>
-						<div class="w-full grid grid-rows-2">
-							<div class="w-full flex items-baseline justify-between">
-								<h2
-									class="text-red-400 text-sm font-semibold uppercase tracking-wider"
-								>
-									Redstone
-								</h2>
-								<span class="text-neutral-400 text-xs font-medium">
-									Owned: {hasRedstone ? data.currencies?.redstone : 0}
-								</span>
-							</div>
-							<Input
-								type="number"
-								bind:value={redstoneAmount}
-								oninput={calculatePotionMix}
-								class="h-8 text-sm"
-								max={hasRedstone ? data.currencies?.redstone : 0}
-								min="0"
+					<div class="relative">
+						<div
+							class="absolute inset-0 bg-primary/40 translate-x-[4px] translate-y-[4px] rounded-xl"
+						></div>
+						<div
+							class="p-4 bg-zinc-950 border-2 border-primary/50 rounded-xl backdrop-blur-2xl flex items-center justify-between gap-x-4"
+						>
+							<img
+								src="/alch-redstone.png"
+								alt="Redstone"
+								class="w-14 h-14 object-contain p-3 bg-red-600/20 border border-red-500/30 rounded-xl"
 							/>
+							<div class="w-full grid grid-rows-2">
+								<div class="w-full flex items-baseline justify-between">
+									<h2
+										class="text-red-400 text-sm font-semibold uppercase tracking-wider"
+									>
+										Redstone
+									</h2>
+									<span class="text-neutral-400 text-xs font-medium">
+										Owned: {hasRedstone ? data.currencies?.redstone : 0}
+									</span>
+								</div>
+								<Input
+									type="number"
+									bind:value={redstoneAmount}
+									oninput={calculatePotionMix}
+									class="h-8 text-sm border-primary"
+									max={hasRedstone ? data.currencies?.redstone : 0}
+									min="0"
+								/>
+							</div>
 						</div>
 					</div>
 
 					<!-- stone-2's card -->
-					<div
-						class="px-4 py-3 bg-white/10 border-2 border-white/10 rounded-xl backdrop-blur-2xl flex items-center justify-between gap-x-4"
-					>
-						<img
-							src="/alch-glowstone.png"
-							alt="Glowstone"
-							class="w-14 h-14 object-contain p-3 bg-yellow-600/20 border border-yellow-500/30 rounded-xl"
-						/>
-						<div class="w-full grid grid-rows-2">
-							<div class="w-full flex items-baseline justify-between">
-								<h2
-									class="text-yellow-400 text-sm font-semibold uppercase tracking-wider"
-								>
-									Glowstone
-								</h2>
-								<span class="text-neutral-400 text-xs font-medium">
-									Owned: {hasGlowstone ? data.currencies?.glowstone : 0}
-								</span>
-							</div>
-							<Input
-								type="number"
-								bind:value={glowstoneAmount}
-								oninput={calculatePotionMix}
-								class="h-8 text-sm"
-								max={hasGlowstone ? data.currencies?.glowstone : 0}
-								min="0"
+					<div class="relative">
+						<div
+							class="absolute inset-0 bg-primary/40 translate-x-[4px] translate-y-[4px] rounded-xl"
+						></div>
+						<div
+							class="p-4 bg-zinc-950 border-2 border-primary/50 rounded-xl backdrop-blur-2xl flex items-center justify-between gap-x-4"
+						>
+							<img
+								src="/alch-glowstone.png"
+								alt="Glowstone"
+								class="w-14 h-14 object-contain p-3 bg-yellow-600/20 border border-yellow-500/30 rounded-xl"
 							/>
+							<div class="w-full grid grid-rows-2">
+								<div class="w-full flex items-baseline justify-between">
+									<h2
+										class="text-yellow-400 text-sm font-semibold uppercase tracking-wider"
+									>
+										Glowstone
+									</h2>
+									<span class="text-neutral-400 text-xs font-medium">
+										Owned: {hasGlowstone ? data.currencies?.glowstone : 0}
+									</span>
+								</div>
+								<Input
+									type="number"
+									bind:value={glowstoneAmount}
+									oninput={calculatePotionMix}
+									class="h-8 text-sm border-primary"
+									max={hasGlowstone ? data.currencies?.glowstone : 0}
+									min="0"
+								/>
+							</div>
 						</div>
 					</div>
 
 					<!-- stone-3's card -->
-					<div
-						class="px-4 py-3 bg-white/10 border-2 border-white/10 rounded-xl backdrop-blur-2xl flex items-center justify-between gap-x-4"
-					>
-						<img
-							src="/alch-aquaregia.png"
-							alt="Aqua Regia"
-							class="w-14 h-14 object-contain p-3 bg-blue-600/20 border border-blue-500/30 rounded-xl"
-						/>
-						<div class="w-full grid grid-rows-2">
-							<div class="w-full flex items-baseline justify-between">
-								<h2
-									class="text-blue-400 text-sm font-semibold uppercase tracking-wider"
-								>
-									Aqua Regia
-								</h2>
-								<span class="text-neutral-400 text-xs font-medium">
-									Owned: {hasAquaRegia ? data.currencies?.aqua_regia : 0}
-								</span>
-							</div>
-							<Input
-								type="number"
-								bind:value={aquaRegiaAmount}
-								oninput={calculatePotionMix}
-								class="h-8 text-sm"
-								max={hasAquaRegia ? data.currencies?.aqua_regia : 0}
-								min="0"
+					<div class="relative">
+						<div
+							class="absolute inset-0 bg-primary/40 translate-x-[4px] translate-y-[4px] rounded-xl"
+						></div>
+						<div
+							class="p-4 bg-zinc-950 border-2 border-primary/50 rounded-xl backdrop-blur-2xl flex items-center justify-between gap-x-4"
+						>
+							<img
+								src="/alch-aquaregia.png"
+								alt="Aqua Regia"
+								class="w-14 h-14 object-contain p-3 bg-blue-600/20 border border-blue-500/30 rounded-xl"
 							/>
+							<div class="w-full grid grid-rows-2">
+								<div class="w-full flex items-baseline justify-between">
+									<h2
+										class="text-blue-400 text-sm font-semibold uppercase tracking-wider"
+									>
+										Aqua Regia
+									</h2>
+									<span class="text-neutral-400 text-xs font-medium">
+										Owned: {hasAquaRegia ? data.currencies?.aqua_regia : 0}
+									</span>
+								</div>
+								<Input
+									type="number"
+									bind:value={aquaRegiaAmount}
+									oninput={calculatePotionMix}
+									class="h-8 text-sm border-primary"
+									max={hasAquaRegia ? data.currencies?.aqua_regia : 0}
+									min="0"
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<!-- second card goes here -->
-			<div class="h-full w-full flex flex-col items-center gap-y-6 py-15">
+			<div class="h-full w-full flex flex-col items-center gap-y-8 py-15">
 				<!-- just a small effect i liked hehe (this was the hardest phew) -->
-
-				<div
-					class="flask bg-black/50 rounded-full p-8 shadow-primary animate-pulse"
-				>
-					<FlaskConical class="h-20 w-20 text-primary " />
+				<div class="relative">
+					<div
+						class="flask absolute inset-0 bg-primary rounded-full animate-pulse -z-10"
+					></div>
+					<div class="relative bg-black/90 rounded-full p-8">
+						<FlaskConical class="z-10 h-20 w-20 text-primary " />
+					</div>
 				</div>
-				<div class="flex flex-col items-center justify-start gap-y-2">
+				<div class="relative group">
+					<div
+						class="absolute inset-0 bg-primary translate-x-1 translate-y-1 rounded-md transition-transform group-hover:translate-x-0.5 group-hover:translate-y-0.5"
+					></div>
 					<Button
 						onclick={updatePotionMix}
-						class="px-10 py-6  font-alchemize shadow-black rounded-full text-xl hover:scale-102 transition"
+						class="relative flex items-center justify-between border-2 border-primary bg-zinc-950 text-primary font-semibold uppercase tracking-widest text-center px-10 py-5 rounded-md transition-transform group-hover:-translate-x-px group-hover:-translate-y-px text-lg"
 					>
+						<span>Convert Stones</span>
 						{#if loading}
 							<div
-								class="loader border-gray-500 border-2 border-t-white rounded-full size-5 animate-spin"
+								class="loader border-gray-500 border-2 border-t-black rounded-full size-5 animate-spin"
 							></div>
 						{/if}
-						<!-- sounds cool eh? -->
-						Convert stones
 					</Button>
 				</div>
 			</div>
@@ -214,31 +235,40 @@
 			<div
 				class="max-h-full w-full flex flex-col gap-y-2 items-start justify-start"
 			>
-				<h2 class="text-muted-foreground uppercase text-sm">Result</h2>
-				<div
-					class="h-full w-full bg-white/10 border-2 rounded-2xl flex flex-col gap-y-4 items-center justify-between py-10 px-5"
-				>
+				<h2 class="text-muted-foreground uppercase text-sm text-right w-full">
+					Result
+				</h2>
+				<div class="relative w-full h-full">
 					<div
-						class="bg-black/50 rounded-full p-5 w-30 border-2 border-primary flex items-center justify-center shadow-primary"
+						class="absolute inset-0 bg-primary/40 translate-x-[4px] translate-y-[4px] rounded-2xl -z-10"
+					></div>
+					<div
+						class="h-full w-full bg-zinc-950 border-2 border-primary/50 rounded-2xl flex flex-col gap-y-4 items-center justify-between py-10 px-5 z-20"
 					>
-						<img src="/Alchemize.png" alt="" class="h-20 text-primary" />
-					</div>
-					<!-- replicated the stone card but made a few chnages like center align -->
-					<div class="w-full flex flex-col items-center justify-start gap-y-3">
-						<h2
-							class="text-red-900 text-shadow-md font-bold text-md uppercase tracking-wider"
+						<div
+							class="bg-black/50 rounded-full p-5 w-30 border-2 border-primary/50 flex items-center justify-center shadow-primary"
 						>
-							Potion Mix
-						</h2>
-						<Input
-							type="number"
-							bind:value={potionMixAmount}
-							class="h-8 text-sm"
-							readonly
-						/>
-						<span class="text-neutral-400 text-xs font-medium">
-							Owned: {hasPotionMix ? data.currencies?.potion_mix : 0}
-						</span>
+							<img src="/Alchemize.png" alt="" class="h-20 text-primary" />
+						</div>
+						<!-- replicated the stone card but made a few chnages like center align -->
+						<div
+							class="w-full flex flex-col items-center justify-start gap-y-3"
+						>
+							<h2
+								class="text-red-900 text-shadow-md font-bold text-md uppercase tracking-wider"
+							>
+								Potion Mix
+							</h2>
+							<Input
+								type="number"
+								bind:value={potionMixAmount}
+								class="h-8 text-sm border-primary"
+								readonly
+							/>
+							<span class="text-neutral-400 text-xs font-medium">
+								Owned: {hasPotionMix ? data.currencies?.potion_mix : 0}
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
